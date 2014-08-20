ff4afa365b2fee494366ce0ab8190c35e7489e00
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2014-08-21 03:05:17 +0900

    add shell, arg parser, query grammar stub

---
+++ src/pegquery/ArgumentsParser.java
@@ -0,0 +1,219 @@
+package pegquery;
+
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+public class ArgumentsParser {
+	/**
+	 * contains recognized options
+	 */
+	protected final List<Option> optionList = new ArrayList<>();
+
+	/**
+	 * key is option name (short name or long name)
+	 */
+	protected final Map<String, Option> optionMap = new HashMap<>();
+
+	private int maxSizeOfUsage;
+
+	/**
+	 * 
+	 * @param shortName
+	 * may be null. not start with '--'
+	 * @param longName
+	 * may be null. not start with '-'
+	 * @param hasArg
+	 * @param description
+	 * may be null
+	 * @param listener
+	 * may be null
+	 * @return
+	 * this
+	 * @throws IllegalArgumentException
+	 */
+	public ArgumentsParser addOption(String shortName, String longName, 
+	                                 boolean hasArg, String description, 
+	                                 OptionListener listener) throws IllegalArgumentException {
+		// check short name format
+		Optional<String> actualShortName = Optional.empty();
+		if(shortName != null) {
+			if(shortName.startsWith("--")) {
+				throw new IllegalArgumentException("illegal short name: " + shortName);
+			}
+			actualShortName = Optional.of(shortName.startsWith("-") ? shortName : "-" + shortName);
+			if(this.optionMap.containsKey(actualShortName.get())) {
+				throw new IllegalArgumentException("found duplicated option: " + actualShortName);
+			}
+		}
+
+		// check long name format
+		Optional<String> actualLongName = Optional.empty();
+		if(longName != null) {
+			if(!longName.startsWith("--") && longName.startsWith("-")) {
+				throw new IllegalArgumentException("illegal long name: " + longName);
+			}
+			actualLongName = Optional.of(longName.startsWith("--") ? longName : "--" + longName);
+			if(this.optionMap.containsKey(actualLongName.get())) {
+				throw new IllegalArgumentException("found duplicated option: " + actualLongName);
+			}
+		}
+
+		// add option
+		Option option = new Option(actualShortName, actualLongName, hasArg, 
+				Optional.ofNullable(listener), Optional.ofNullable(description));
+		this.optionList.add(option);
+		actualShortName.ifPresent(key -> this.optionMap.put(key, option));
+		actualLongName.ifPresent(key -> this.optionMap.put(key, option));
+
+		int usageSize = option.getUsage().length();
+		if(this.maxSizeOfUsage < usageSize) {
+			this.maxSizeOfUsage = usageSize;
+		}
+		return this;
+	}
+
+	public void parseAndInvokeAction(String[] args) throws IllegalArgumentException {
+		final List<Pair<Optional<String>, Optional<OptionListener>>> pairList = new ArrayList<>();
+
+		// parse arguments
+		final int size = args.length;
+		final Set<Option> foundOptionSet = new HashSet<>();
+		for(int i = 0; i < size; i++) {
+			String optionSymbol = args[i];
+			Option option = this.optionMap.get(optionSymbol);
+			if(option == null) {
+				throw new IllegalArgumentException("illegal option: " + optionSymbol);
+			}
+			if(foundOptionSet.contains(option)) {
+				throw new IllegalArgumentException("duplicated option: " + optionSymbol);
+			}
+			Optional<String> arg = Optional.empty();
+			if(option.requireArg()) {
+				if(++i < size && !args[i].startsWith("-")) {
+					arg = Optional.of(args[i]);
+				}
+				else {
+					throw new IllegalArgumentException("require argument: " + optionSymbol);
+				}
+			}
+			pairList.add(new Pair<Optional<String>, Optional<OptionListener>>(arg, option.getListener()));
+			foundOptionSet.add(option);
+		}
+
+		// invoke action
+		for(final Pair<Optional<String>, Optional<OptionListener>> pair : pairList) {
+			pair.getRight().ifPresent(a -> a.invoke(pair.getLeft()));
+		}
+	}
+
+	public void printHelpBeforeExit(PrintStream stream, int status) {
+		StringBuilder sBuilder = new StringBuilder();
+		for(int i = 0; i < this.maxSizeOfUsage; i++) {
+			sBuilder.append(' ');
+		}
+		String spaces = sBuilder.toString();
+
+		// format help message
+		sBuilder = new StringBuilder();
+		for(Option option : this.optionList) {
+			final int size = option.getUsage().length();
+			sBuilder.append("    ");
+			sBuilder.append(option.getUsage());
+			for(int i = 0; i < this.maxSizeOfUsage - size; i++) {
+				sBuilder.append(' ');
+			}
+			Optional<String> desc = option.getDescription();
+			if(desc.isPresent()) {
+				String[] descs = desc.get().split("\n");
+				for(int i = 0; i < descs.length; i++) {
+					if(i > 0) {
+						sBuilder.append(System.lineSeparator());
+						sBuilder.append(spaces);
+					}
+					sBuilder.append("    ");
+					sBuilder.append(descs[i]);
+				}
+			}
+			sBuilder.append(System.lineSeparator());
+		}
+		stream.print(sBuilder.toString());
+		System.exit(status);
+	}
+
+	protected static class Option {
+		protected final Optional<String> shortName;
+		protected final Optional<String> longName;
+		protected final boolean hasArg;
+		protected final Optional<OptionListener> listener;
+		protected final Optional<String> description;
+
+		protected final String usage;
+
+		/**
+		 * 
+		 * @param shortName
+		 * starts with '-'
+		 * @param longName
+		 * starts with '--'
+		 * @param hasArg
+		 * @param listener
+		 * not null
+		 * @param description
+		 * not null
+		 */
+		public Option(Optional<String> shortName, Optional<String> longName, boolean hasArg, 
+		              Optional<OptionListener> listener, Optional<String> description) {
+			this.shortName = shortName;
+			this.longName = longName;
+			this.hasArg = hasArg;
+			this.listener = listener;
+			this.description = description;
+
+			final StringBuilder sBuilder = new StringBuilder();
+			this.shortName.ifPresent(n -> sBuilder.append(n));
+			if(this.shortName.isPresent() && this.longName.isPresent()) {
+				sBuilder.append(" | ");
+			}
+			this.longName.ifPresent(n -> sBuilder.append(n));
+			if(this.hasArg) {
+				sBuilder.append(" <arg>");
+			}
+			this.usage = sBuilder.toString();
+		}
+
+		public Optional<String> getShortName() {
+			return this.shortName;
+		}
+
+		public Optional<String> getLongName() {
+			return this.longName;
+		}
+
+		public boolean requireArg() {
+			return this.hasArg;
+		}
+
+		public Optional<OptionListener> getListener() {
+			return this.listener;
+		}
+
+		public Optional<String> getDescription() {
+			return this.description;
+		}
+
+		public String getUsage() {
+			return this.usage;
+		}
+	}
+
+	@FunctionalInterface
+	protected static interface OptionListener {
+		public void invoke(Optional<String> arg);
+	}
+}
--- src/pegquery/Main.java
+++ src/pegquery/Main.java
@@ -1,544 +1,66 @@
 package pegquery;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.util.Optional;
 
-public class Main {
-	public final static String  ProgName  = "PEG4d";
-	public final static String  CodeName  = "yokohama";
-	public final static int     MajorVersion = 1;
-	public final static int     MinerVersion = 0;
-	public final static int     PatchLevel   = 0;
-	public final static String  Version = "" + MajorVersion + "." + MinerVersion + "." + PatchLevel;
-	public final static String  Copyright = "Copyright (c) 2014, Konoha4e project authors";
-	public final static String  License = "BSD-Style Open Source";
+import org.peg4d.Grammar;
+import org.peg4d.GrammarComposer;
+import org.peg4d.ParserContext;
+import org.peg4d.ParsingObject;
 
-	// -p konoha.peg
-	private static String GrammarFile = null; // default
+public class Main {	//TODO: pipe line mode
+	private final static String defaultStartPoint = "TopLevel";
+	private static Optional<String> pegDef4Data = Optional.empty();
+	private static Optional<String> targetData = Optional.empty();
+	private final static String pegDef4Query = "query.peg";
+	private static Optional<String> queryScript = Optional.empty();
 
-	// -s StartingPoint
-	private static String StartingPoint = "File";  // default
+	public static void main(String[] args) {
+		/**
+		 * parse argument
+		 */
+		final ArgumentsParser argsParser = new ArgumentsParser();
+		argsParser
+		.addOption("h", "help", false, "show this help message", 
+				s -> argsParser.printHelpBeforeExit(System.out, 0))
+		.addOption("g", "grammar", true, "peg definition of target data format", 
+				s -> pegDef4Data = Optional.of(s.get()))
+		.addOption("t", "target", true, "target data file", 
+				s -> targetData = Optional.of(s.get()))
+		.addOption("q", "query", true, "query script", 
+				s -> queryScript = Optional.of(s.get()));
 
-	// -t output
-	private static String OutputType = "pego";  // default
-
-	// -f format
-	private static String PegFormat = null;  // default
-
-	// -i
-	private static boolean ShellMode = false;
-
-	// -c
-	public static boolean RecognitionOnlyMode = false;
-
-	//
-	private static String InputFileName = null;
-	
-	// -o
-	private static String OutputFileName = null;
-
-	// --verbose
-	public static boolean VerboseMode    = false;
-
-	// --verbose:peg
-	public static boolean VerbosePeg = false;
-
-	// --test
-	public static boolean TestMode = false;
-
-	// --a
-	public static boolean DiskMode = false;
-	
-	// --verbose:stat
-	public static int     StatLevel = -1;
-
-	// --static => false
-	public static String  ParserName = null;
-	public static boolean TracingMemo = true;
-	public static boolean UseFifo = false;
-	public static boolean AllExpressionMemo  = true;
-	public static boolean PackratStyleMemo   = false;
-	public static boolean ObjectFocusedMemo  = false;
-	
-	// -O
-	public static int OptimizationLevel = 2;
-	public static int MemoFactor = 256;
-	public static String CSVFileName = "results.csv";
-
-	public final static void main(String[] args) {
-		parseCommandArguments(args);
-		Grammar peg = GrammarFile == null ? Grammar.PEG4d : Grammar.load(new GrammarComposer(), GrammarFile);
-		if(PegFormat != null) {
-			Formatter fmt = loadFormatter(PegFormat);
-			peg.show(StartingPoint, fmt);
-			return;
-		}
-		if(InputFileName != null) {
-			loadInputFile(peg, InputFileName);
-		}
-		else {
-			ShellMode = true;
-		}
-		if(ShellMode) {
-			performShell(peg);
-		}
-	}
-	
-	private static void parseCommandArguments(String[] args) {
-		int index = 0;
-		while (index < args.length) {
-			String argument = args[index];
-			if (!argument.startsWith("-")) {
-				break;
-			}
-			index = index + 1;
-			if ((argument.equals("-p") || argument.equals("--peg")) && (index < args.length)) {
-				GrammarFile = args[index];
-				index = index + 1;
-			}
-			else if ((argument.equals("-s") || argument.equals("--start")) && (index < args.length)) {
-				StartingPoint = args[index];
-				index = index + 1;
-			}
-			else if ((argument.equals("-f") || argument.equals("--format")) && (index < args.length)) {
-				PegFormat = args[index];
-				index = index + 1;
-			}
-			else if(argument.startsWith("-M")) {
-				Main.MemoFactor  = UCharset.parseInt(argument.substring(2), 256);
-			}
-			else if (argument.startsWith("-O")) {
-				OptimizationLevel = UCharset.parseInt(argument.substring(2), 2);
-			}
-			else if (argument.equals("-i")) {
-				Main.OptimizationLevel = 0;
-				ShellMode = true;
-			}
-			else if (argument.equals("-c")) {
-				RecognitionOnlyMode = true;
-			}
-			else if (argument.equals("-a")) {
-				DiskMode = true;
-			}
-			else if(argument.startsWith("--test")) {
-				TestMode = true;
-			}
-			else if(argument.startsWith("--stat")) {
-				StatLevel = UCharset.parseInt(argument.substring(6), 1);
-				OutputType = "none";
-			}
-			else if(argument.startsWith("--csv") && (index < args.length)) {
-				CSVFileName = args[index];
-				if(!CSVFileName.endsWith(".csv")) {
-					Main._Exit(1, "invalid csv file: " + CSVFileName);
-				}
-				index = index + 1;
-			}
-			else if ((argument.equals("-t") || argument.equals("--target")) && (index < args.length)) {
-				OutputType = args[index];
-				index = index + 1;
-			}
-			else if ((argument.equals("-o") || argument.equals("--out")) && (index < args.length)) {
-				OutputFileName = args[index];
-				if(OutputFileName.endsWith(".csv")) {
-					OutputType = "csv";
-				}
-				if(OutputFileName.endsWith(".json")) {
-					OutputType = "json";
-				}
-				index = index + 1;
-			}
-			else if (argument.equals("--name")) {
-				ParserName = args[index];
-				index = index + 1;
-			}
-			else if(argument.startsWith("--memo")) {
-				if(argument.equals("--memo:packrat")) {
-					AllExpressionMemo = false;
-					PackratStyleMemo = true;
-					ObjectFocusedMemo = false;
-				}
-				else if(argument.equals("--memo:data")) {
-					AllExpressionMemo = false;
-					PackratStyleMemo = false;
-					ObjectFocusedMemo = true;
-				}
-				else if(argument.equals("--memo:none")) {
-					AllExpressionMemo = false;
-					PackratStyleMemo = false;
-					ObjectFocusedMemo = false;
-					TracingMemo = false;
-				}
-				else if(argument.equals("--memo:static")) {
-					TracingMemo = false;
-				}
-				else if(argument.equals("--memo:fifo")) {
-					UseFifo = true;
-				}
-				else {
-					ShowUsage("unknown option: " + argument);
-				}
-			}
-			else if(argument.startsWith("--verbose")) {
-				VerboseMode = true;
-			}
-			else {
-				ShowUsage("unknown option: " + argument);
-			}
-		}
-		if (index < args.length) {
-			InputFileName = args[index];
-			index++;
-		}
-		else {
-			Main.OptimizationLevel = 0;
-			ShellMode = true;
-		}
-	}
-
-	public final static void ShowUsage(String Message) {
-		System.out.println(ProgName + " :");
-		System.out.println("  -p <FILE>                 Specify PEG file  default: PEG4d grammar");
-		System.out.println("  -s | --start <NAME>       Specify Non-Terminal as the starting point. default: TopLevel");
-		System.out.println("  -t <type>                 Specify output type. default: pego");
-		System.out.println("     pego|none|json|csv");
-		System.out.println("  -c                        Invoke as checker (without output generation). Exit 1 when failed");
-		System.out.println("  -f | --format<type>       Specify PEG formatter");
-		System.out.println("  --packrat                 Packrat Parser");
-		System.out.println("  -M<num>                   Memo Factor -M0 => No Memo");
-		System.out.println("  --verbose                 Printing Debug infomation");
-		System.out.println("  --verbose:peg             Printing Peg/Debug infomation");
-		Main._Exit(0, Message);
-	}
-
-	private final static UMap<Class<?>> driverMap = new UMap<Class<?>>();
-	static {
-		driverMap.put("peg4d", Formatter.class);
-		//driverMap.put("pegjs", PegJSFormatter.class);
-	}
-
-	private static Formatter loadDriverImpl(String driverName) {
 		try {
-			return (Formatter) driverMap.get(driverName).newInstance();
+			argsParser.parseAndInvokeAction(args);
 		}
-		catch(Exception e) {
+		catch(IllegalArgumentException e) {
+			System.err.println(e.getMessage());
+			argsParser.printHelpBeforeExit(System.err, 1);
 		}
-		return null;
-	}
-	
-	private static Formatter loadFormatter(String driverName) {
-		Formatter d = loadDriverImpl(driverName);
-		if(d == null) {
-			System.out.println("Supported formatter list:");
-			UList<String> driverList = driverMap.keys();
-			for(int i = 0; i < driverList.size(); i++) {
-				String k = driverList.ArrayValues[i];
-				d = loadDriverImpl(k);
-				if(d != null) {
-					System.out.println("\t" + k + " - " + d.getDesc());
-				}
-			}
-			Main._Exit(1, "undefined formatter: " + driverName);
-		}
-		return d;
-	}
 
-	private synchronized static void loadInputFile(Grammar peg, String fileName) {
-		String startPoint = StartingPoint;
-		Main.printVerbose("FileName", fileName);
-		Main.printVerbose("Grammar", peg.getName());
-		Main.printVerbose("StartingPoint", StartingPoint);
-		ParserContext p = peg.newParserContext(Main.loadSource(peg, fileName));
-		if(Main.StatLevel == 0) {
-			long t = System.currentTimeMillis();
-			p.setRecognitionOnly(true);
-			while(System.currentTimeMillis()-t < 4000) {
-				System.out.print(".");System.out.flush();
-				p.parseNode(startPoint);
-				p.sourcePosition = 0;
-			}
-			p.setRecognitionOnly(false);
-			p.initMemo();
-			System.gc();
-			try{
-				Thread.sleep(500);
-			}catch(InterruptedException e){
-			}
-			System.out.println(" GO!!");
-		}
-		//while(p.hasNode()) {
-		p.beginPeformStat();
-		ParsingObject pego = p.parseNode(startPoint);
-		p.endPerformStat(pego);
-		//}
-		if(p.hasChar()) {
-			long pos = p.getPosition();
-			if(pos > 0) {
-				p.showPosition("consumed", pos-1);
-			}
-			p.showPosition("unconsumed", pos);
-		}
-		if(OutputType.equalsIgnoreCase("pego")) {
-			new Generator(OutputFileName).writePego(pego);
-		}
-		else if(OutputType.equalsIgnoreCase("json")) {
-			new Generator(OutputFileName).writeJSON(pego);
-		}
-		else if(OutputType.equalsIgnoreCase("csv")) {
-			new Generator(OutputFileName).writeCommaSeparateValue(pego, 0.9);
-		}
-	}
+		/**
+		 * parse data file
+		 */
+		Grammar peg = Grammar.load(new GrammarComposer(), pegDef4Data.get());
+		ParserContext p = peg.newParserContext(org.peg4d.Main.loadSource(peg, targetData.get()));
+		ParsingObject parsedObject = p.parseNode(defaultStartPoint);	//FIXME
 
-	public final static void performShell(Grammar peg) {
-		Main._PrintLine(ProgName + Version + " (" + CodeName + ") on " + Main._GetPlatform());
-		Main._PrintLine(Copyright);
-		Main._PrintLine("Tips: \\Name to switch the starting point to Name");
-		int linenum = 1;
-		String line = null;
-		String startPoint = StartingPoint;
-		while ((line = readMultiLine(startPoint + ">>> ", "    ")) != null) {
-			if(line.startsWith("\\")) {
-				startPoint = switchStaringPoint(peg, line.substring(1), startPoint);
-				continue;
-			}
-			ParsingSource source = new StringSource(peg, "(stdin)", linenum, line);
-			ParserContext p = peg.newParserContext(source);
-			ParsingObject pego = p.parseNode(startPoint);
-			System.out.println("Parsed: " + pego);
-			linenum = linenum + 1;
-		}
-		System.out.println("");
-	}
-	
-	private static String switchStaringPoint(Grammar peg, String ruleName, String startPoint) {
-		if(peg.hasRule(ruleName)) {
-			peg.show(ruleName);
-			return ruleName;
-		}
-		UList<String> list = peg.ruleMap.keys();
-		System.out.print("Choose:");
-		for(int i = 0; i < list.size(); i++) {
-			System.out.print(" " + list.ArrayValues[i]);
-		}
-		System.out.println("");
-		return startPoint;
-	}
-
-	private static jline.ConsoleReader ConsoleReader = null;
-
-	private final static String readMultiLine(String prompt, String prompt2) {
-		if(ConsoleReader == null) {
-			try {
-				ConsoleReader = new jline.ConsoleReader();
-			}
-			catch(IOException e) {
-				throw new RuntimeException(e);
-			}
-		}
-		String line = readSingleLine(prompt);
-		if(line == null) {
-			System.exit(0);
-		}
-		if(prompt2 != null) {
-			int level = 0;
-			while((level = CheckBraceLevel(line)) > 0) {
-				String line2 = readSingleLine(prompt2);
-				line += "\n" + line2;
-			}
-			if(level < 0) {
-				line = "";
-				Main._PrintLine(" .. canceled");
-			}
-		}
-		ConsoleReader.getHistory().addToHistory(line);
-		return line;
-	}
-
-	private final static String readSingleLine(String prompt) {
-		try {
-			return ConsoleReader.readLine(prompt);
-		}
-		catch(IOException e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-
-	private static void PrintStackTrace(Exception e, long linenum) {
-		StackTraceElement[] elements = e.getStackTrace();
-		int size = elements.length + 1;
-		StackTraceElement[] newElements = new StackTraceElement[size];
-		int i = 0;
-		for(; i < size; i++) {
-			if(i == size - 1) {
-				newElements[i] = new StackTraceElement("<TopLevel>", "TopLevelEval", "stdin", (int)linenum);
-				break;
-			}
-			newElements[i] = elements[i];
-		}
-		e.setStackTrace(newElements);
-		e.printStackTrace();
-	}
-
-	private final static int CheckBraceLevel(String Text) {
-		int level = 0;
-		for(int i = 0; i < Text.length(); i++) {
-			char ch = Text.charAt(i);
-			if(ch == '{') {
-				level++;
-			}
-			if(ch == '}') {
-				level--;
-			}
-		}
-		return level;
-	}
-
-	// file
-
-	public final static ParsingSource loadSource(Grammar peg, String fileName) {
-		InputStream Stream = Main.class.getResourceAsStream("/" + fileName);
-		if (Stream == null) {
-			try {
-				File f = new File(fileName);
-				if(f.length() > 128 * 1024) {
-					return new FileSource(peg, fileName);
-				}
-				Stream = new FileInputStream(fileName);
-			} catch (FileNotFoundException e) {
-				Main._Exit(1, "file not found: " + fileName);
-				return null;
-			}
-		}
-		BufferedReader reader = new BufferedReader(new InputStreamReader(Stream));
-		try {
-			StringBuilder builder = new StringBuilder();
-			String line = reader.readLine();
-			while(line != null) {
-				builder.append(line);
-				builder.append("\n");
-				line = reader.readLine();
-			}
-			return new StringSource(peg, fileName, 1, builder.toString());
-		}
-		catch(IOException e) {
-			e.printStackTrace();
-			Main._Exit(1, "file error: " + fileName);
-		}
-		return null;
-	}
-
-	public final static String _GetPlatform() {
-		return "Java JVM-" + System.getProperty("java.version");
-	}
-
-//	public final static String _GetEnv(String Name) {
-//		return System.getenv(Name);
-//	}
-//
-//	public final static void _Print(Object msg) {
-//		System.err.print(msg);
-//	}
-
-	public final static void _PrintLine(Object message) {
-		System.err.println(message);
-	}
-
-	public final static void printVerbose(String head, Object message) {
-		if(Main.VerboseMode) {
-			System.out.println(head + ": " + message);
-		}
-	}
-
-	
-	
-	public final static void _Exit(int status, String message) {
-		if(Main.VerboseMode) {
-			System.err.println("EXIT " + Main._GetStackInfo(3) + " " + message);
+		/**
+		 * exec query
+		 */
+		Grammar queryPeg = Grammar.load(new GrammarComposer(), pegDef4Query);
+		if(queryScript.isPresent()) {
+			//TODO: load query script
 		}
 		else {
-			System.err.println("EXIT " + message);
-		}
-		System.exit(status);
-	}
-
-	public static boolean DebugMode = false;
-
-	public final static void _PrintDebug(String msg) {
-		if(Main.DebugMode) {
-			_PrintLine("DEBUG " + Main._GetStackInfo(3) + ": " + msg);
-		}
-	}
-
-	public final static String _GetStackInfo(int depth) {
-		String LineNumber = " ";
-		Exception e =  new Exception();
-		StackTraceElement[] Elements = e.getStackTrace();
-		if(depth < Elements.length) {
-			StackTraceElement elem = Elements[depth];
-			LineNumber += elem;
-		}
-		return LineNumber;
-	}
-
-	public final static boolean _IsFlag(int flag, int flag2) {
-		return ((flag & flag2) == flag2);
-	}
-
-	public final static int _UnsetFlag(int flag, int flag2) {
-		return (flag & (~flag2));
-	}
-
-	public final static char _GetChar(String Text, int Pos) {
-		return Text.charAt(Pos);
-	}
-
-	public final static String _CharToString(int ch) {
-			return String.format("%c", ch);
-	}
-
-	public static String _SourceBuilderToString(UStringBuilder sb) {
-		return Main._SourceBuilderToString(sb, 0, sb.slist.size());
-	}
-
-	public static String _SourceBuilderToString(UStringBuilder sb, int beginIndex, int endIndex) {
-		StringBuilder jsb = new StringBuilder();
-		for(int i = beginIndex; i < endIndex; i = i + 1) {
-			jsb.append(sb.slist.ArrayValues[i]);
-		}
-		return jsb.toString();
-	}
-
-	public final static void _WriteFile(String fileName, UList<UStringBuilder> list) {
-		if(fileName == null) {
-			for(int i = 0; i < list.size(); i++) {
-				UStringBuilder sb = list.ArrayValues[i];
-				System.out.println(sb.toString());
-				sb.clear();
-			}
-		}
-		else {
-			try {
-				BufferedWriter w = new BufferedWriter(new FileWriter(fileName));
-				for(int i = 0; i < list.size(); i++) {
-					UStringBuilder sb = list.ArrayValues[i];
-					w.write(sb.toString());
-					w.write("\n\n");
-					sb.clear();
+			Shell shell = new Shell();
+			Pair<String, Integer> resultPair = null;
+			while((resultPair = shell.readLine()) != null) {	//TODO:
+				if(resultPair.getLeft().equals("")) {
+					continue;
 				}
-				w.close();
+				System.out.println(resultPair.getLeft());
 			}
-			catch(IOException e) {
-				_Exit(1, "cannot to write: " + e);
-			}
+			System.out.println("");
 		}
 	}
-
 }
